5bdb30e9b96cf2dbccfb232f41b9b435958bba11
Rui Huang <huangrui2012@outlook.com>
2025-07-09 16:58:09 +0100

    commit

--- Leetcode/13. Roman to Integer.py
+++ Leetcode/13. Roman to Integer.py
@@ -21,3 +21,18 @@ C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
 
 '''
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+             'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev = 0
+        for i in reversed(s):
+            value = roman[i]
+            if value < prev:
+                total -= value
+            else:
+                total += value
+                prev = value
+        return total
