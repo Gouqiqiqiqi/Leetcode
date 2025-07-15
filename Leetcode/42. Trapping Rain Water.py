b7f5b6a2b91d818a509f287d6e365bfc07747ee6
Rui Huang <huangrui2012@outlook.com>
2025-07-15 20:58:57 +0100

    commit

--- Leetcode/42. Trapping Rain Water.py
+++ Leetcode/42. Trapping Rain Water.py
@@ -26,6 +26,7 @@ class Solution:
         n = len(height)
         max_left = [0] * n
         max_right = [0] * n
+        water = 0
 
         for i in range(n):
             j = -i - 1
@@ -33,8 +34,7 @@ class Solution:
             left_wall = max(left_wall, height[i])
             max_right[j] = right_wall
             right_wall = max(right_wall, height[j])
-
-        water = 0
+            
         for i in range(n):
             pot = min(max_left[i], max_right[i])
             water += max(0, pot - height[i])
