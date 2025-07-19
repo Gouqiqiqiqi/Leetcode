6143e06928af8238fe913276a057fb9623364362
Rui Huang <huangrui2012@outlook.com>
2025-07-19 12:48:00 +0100

    commit

--- Leetcode/13. Roman to Integer.py
+++ Leetcode/13. Roman to Integer.py
@@ -26,13 +26,13 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
              'C': 100, 'D': 500, 'M': 1000}
+        
+        n = len(s)
         total = 0
-        prev = 0
-        for i in reversed(s):
-            value = roman[i]
-            if value < prev:
-                total -= value
+
+        for i in range (n):
+            if i + 1 < n and roman[s[i]] < roman[s[i + 1]]:
+                total -= roman[s[i]]
             else:
-                total += value
-                prev = value
+                total += roman[s[i]]
         return total
